aa582fabc249cb5a0332bfaafef163ed80d36002
Yury <matatov1989@gmail.com>
2023-06-09 13:46:08 +0300

    init buttons

--- app/src/main/java/com/example/projemanag/IntroActivity.kt
+++ app/src/main/java/com/example/projemanag/IntroActivity.kt
@@ -3,15 +3,28 @@ package com.example.projemanag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.TextView
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var btnSignInIntro: Button
+    private lateinit var btnSignUpIntro: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        initView()
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+    }
+
+    private fun initView() {
+        btnSignInIntro = findViewById(R.id.btnSignInIntro)
+        btnSignUpIntro = findViewById(R.id.btnSignUpIntro)
     }
 }
