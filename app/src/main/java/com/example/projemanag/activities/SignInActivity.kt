61640024e5ec53933f996f2adc4e43b6dad4d692
Yury <matatov1989@gmail.com>
2023-06-10 14:37:26 +0300

    signing in the user

--- app/src/main/java/com/example/projemanag/activities/SignInActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/SignInActivity.kt
@@ -1,26 +1,35 @@
 package com.example.projemanag.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar;
 import com.example.projemanag.R
+import com.google.firebase.auth.FirebaseAuth
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
 
     private lateinit var toolbarSignInActivity: Toolbar
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
     private lateinit var btnSignIn: Button
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        auth = FirebaseAuth.getInstance()
+
         initView()
         setupActionBar()
+        initClickListeners()
     }
 
     private fun initView() {
@@ -41,9 +50,61 @@ class SignInActivity : AppCompatActivity() {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_24)
         }
+    }
 
+    private fun initClickListeners() {
         toolbarSignInActivity.setNavigationOnClickListener {
             onBackPressed()
+        }
+
+        btnSignIn.setOnClickListener {
+            signInRegisterUser()
+        }
+    }
+
+    private fun signInRegisterUser() {
+        val email: String = etEmail.text.toString().trim { it <= ' ' }
+        val password: String = etPassword.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            showProgressDialog(resources.getString(R.string.please_wait))
+            // Sign-In using FirebaseAuth
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(
+                            this@SignInActivity,
+                            "You have successfully signed in.",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(
+                            this@SignInActivity,
+                            task.exception!!.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun validateForm(email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter a e-mail address")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> {
+                true
+            }
         }
     }
 }
