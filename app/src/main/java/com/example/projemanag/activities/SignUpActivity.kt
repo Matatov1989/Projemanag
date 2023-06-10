0e4e2635e5d93c4e0ceeebf08024f5f514b3bc02
Yury <matatov1989@gmail.com>
2023-06-10 14:04:31 +0300

    preparing the signup feature in firebase and in te app

--- app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
@@ -2,13 +2,15 @@ package com.example.projemanag.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar;
 import com.example.projemanag.R
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
 
     private lateinit var toolbarSignUpActivity: Toolbar
     private lateinit var etName: EditText
@@ -19,9 +21,9 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
         initView()
         setupActionBar()
+        initClickListeners()
     }
 
     private fun initView() {
@@ -44,9 +46,44 @@ class SignUpActivity : AppCompatActivity() {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_24)
         }
+    }
 
+    private fun initClickListeners() {
         toolbarSignUpActivity.setNavigationOnClickListener {
             onBackPressed()
+        }
+
+        btnSignUp.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val name: String = etName.text.toString().trim { it <= ' ' }
+        val email: String = etEmail.text.toString().trim { it <= ' ' }
+        val password: String = etPassword.text.toString().trim { it <= ' ' }
+
+        if (validateForm(name, email, password)) {
+            Toast.makeText(this@SignUpActivity, "Now we can register a new user.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter a e-mail address")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            } else -> {
+                true
+            }
         }
     }
 }
