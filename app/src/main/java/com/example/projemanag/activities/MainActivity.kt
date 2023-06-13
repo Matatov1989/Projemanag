d7854f3660d7e73527b0087bab2deb80db3bf3b3
Yury <matatov1989@gmail.com>
2023-06-13 09:26:02 +0300

    adding the drawer functionality

--- app/src/main/java/com/example/projemanag/activities/MainActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/MainActivity.kt
@@ -1,12 +1,76 @@
 package com.example.projemanag.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.projemanag.R
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var toolbarMainActivity: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        setupActionBar()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    private fun initView() {
+        toolbarMainActivity = findViewById(R.id.toolbarMainActivity)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navView = findViewById(R.id.navView)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbarMainActivity)
+        toolbarMainActivity.setNavigationIcon(R.drawable.baseline_menu_24)
+
+        toolbarMainActivity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_my_profile -> {
+
+            }
+            R.id.nav_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
