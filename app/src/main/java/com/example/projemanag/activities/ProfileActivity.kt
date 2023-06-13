9637bda7851db9462ccd30f7cdc358b5c0280053
Yury <matatov1989@gmail.com>
2023-06-13 11:52:59 +0300

    image choosing for the profile image

--- app/src/main/java/com/example/projemanag/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/ProfileActivity.kt
@@ -1,15 +1,25 @@
 package com.example.projemanag.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.projemanag.R
 import com.example.projemanag.firebase.FireStoreClass
 import com.example.projemanag.models.User
 import de.hdodenhof.circleimageview.CircleImageView
+import java.io.IOException
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -20,12 +30,20 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var etMobile: EditText
     private lateinit var btnUpdate: Button
 
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mSelectedImageFileUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
         initView()
         setupActionBar()
+        initClickListeners()
 
         FireStoreClass().loadUserData(this)
 
@@ -51,6 +69,71 @@ class ProfileActivity : AppCompatActivity() {
 
         toolbarPprofileActivity.setNavigationOnClickListener { onBackPressed() }
 
+    }
+
+    private fun initClickListeners() {
+        ivUserImage.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                showImageChooser()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                showImageChooser()
+
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    "Oops, you just denied the permission for storage. You can also allow it from settings.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun showImageChooser() {
+        var galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE_REQUEST_CODE && data!!.data != null) {
+            mSelectedImageFileUri = data.data
+
+            try {
+                Glide
+                    .with(this@ProfileActivity)
+                    .load(mSelectedImageFileUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(ivUserImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
     }
 
     fun setUserDataInUI(user: User) {
